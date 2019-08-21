d96717ec3c2fe1c0e97d83fd758f7827d14dc3b7
nishagbkar <nisha.ee.iitb@gmail.com>
2019-08-22 01:54:47 +0530

    Updates build.sh file in DIB element
    Updates the ELEMENTS_PATH in build.sh file of DIB
    inorder to pass the ironic-python-agent-buildimage-dib test.
    
    Change-Id: Ib20ae0c9add5a20e82f46c5cd1dd48d92d0a4849
    Task: 36323
    Story: 2005114

--- dib/build.sh
+++ dib/build.sh
@@ -2,7 +2,7 @@
 
 #The path for location of ironic-python-agent-ramdisk in ironic-python-image-builder
 #is set using environment variable.
-export ELEMENTS_PATH=${ELEMENTS_PATH:-`.`}
+export ELEMENTS_PATH=${ELEMENTS_PATH:-.}
 
 #The command to build image using disk-image-builder.
 #Run the command to build the image giving the argument(OS name)

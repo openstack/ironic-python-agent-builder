9da0cf41d446e11651a5bd8b111cfd13a02d76b6
Dmitry Tantsur <dtantsur@protonmail.com>
2023-12-08 15:38:23 +0100

    Fix building images for Bullseye and Bookworm
    Bullseye requires installing lshw from backports because the default one
    has broken JSON output:
    https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=1002025
    
    Bookworm requires installing firmware from a different repository.
    
    Change-Id: I1bfecfcaff1547f99fcb48cc54b8f7ff77f0b0d9

--- dib/ironic-python-agent-ramdisk/environment.d/01-debian-ipa.bash
+++ dib/ironic-python-agent-ramdisk/environment.d/01-debian-ipa.bash
@@ -1,5 +1,10 @@
 # IPA is built with non-free firmware by default.
-export DIB_DEBIAN_COMPONENTS=${DIB_DEBIAN_COMPONENTS:-main,contrib,non-free}
+DIB_DEBIAN_NON_FREE=non-free-firmware
+if [[ "$DIB_RELEASE" == "bullseye" ]]; then
+    # Starting with bookworm, firmware is in a separate repository
+    DIB_DEBIAN_NON_FREE=non-free
+fi
+export DIB_DEBIAN_COMPONENTS=${DIB_DEBIAN_COMPONENTS:-main,contrib,$DIB_DEBIAN_NON_FREE}
 
 if [[ $DIB_DEBIAN_COMPONENTS =~ non-free ]]; then
     export IPA_DEBIAN_NONFREE=true

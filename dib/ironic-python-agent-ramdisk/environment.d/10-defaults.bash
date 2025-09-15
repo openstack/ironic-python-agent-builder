5329e3434accd0522f0e209bb80ee5b115513eca
Julia Kreger <juliaashleykreger@gmail.com>
2025-09-15 12:05:06 -0700

    Add additional firmware images which are not needed
    Recently it appears that some newer hardware greatly restricts
    the overall amount of memory available to boot the kernel, and
    as an intermediate solution we aggressively prune out wireless,
    bluetooth, and other non-ethernet firmware images from the
    list of firmware to exclude, in order to maximize savings
    and reduce the overall memory footprint required before
    the kernel boots.
    
    Change-Id: I8f32124769b62cbfca0dc54149b3620f342fc78a
    Signed-off-by: Julia Kreger <juliaashleykreger@gmail.com>

--- dib/ironic-python-agent-ramdisk/environment.d/10-defaults.bash
+++ dib/ironic-python-agent-ramdisk/environment.d/10-defaults.bash
@@ -1,4 +1,4 @@
-export IPA_REMOVE_FIRMWARE=${IPA_REMOVE_FIRMWARE:-amdgpu,netronome,qcom,ti-communication,ti-keystone,ueagle-atm,rsi,mrvl,brcm,mediatek,ath10k,rtlwifi,rtw88,rtw89,libertas,ath11k,mellanox/mlxsw_spectrum}
+export IPA_REMOVE_FIRMWARE=${IPA_REMOVE_FIRMWARE:-amdgpu,netronome,qcom,ti-communication,ti-keystone,ueagle-atm,rsi,mrvl,brcm,mediatek,ath10k,rtlwifi,rtw88,rtw89,libertas,ath11k,mellanox/mlxsw_spectrum,s5p-mfc,ti/tas2781,ti-connectivity,TAS,ath3k,ath6k,ath9k,ath12k,rtl_bt,v4l,cirrus,dvb}
 
 # NOTE(TheJulia): List of what each item represents for future context
 # amdgpu == AMD/ATI Radeon/Vega/Raven firmware for drivers
@@ -18,3 +18,15 @@ export IPA_REMOVE_FIRMWARE=${IPA_REMOVE_FIRMWARE:-amdgpu,netronome,qcom,ti-commu
 # libertas == Marvell libertas wifi
 # auth11k == Qualcomm atheros WLAN
 # mellanox/mlxsw_spectrum = Mellanox Spectrum Switch ASIC
+# s5p-mfc = Samsung MFC SoC video codecs
+# ti/tas2781 = Texas Insturments TAS2781 Notebook audio DSPs.
+# ti-connectivity = Texas Insturments Wi-Fi/BT/FM/GPS
+# TAS = Texas Insturments TAS2781 Notebook audio DSPs.
+# ath3k = Qualcomm 802.11
+# ath6k = Qualcomm 802.11
+# ath9k = Qualcomm 802.11 PCI/PCI-E
+# ath12k = Qualcomm Wi-Fi 7
+# rtl_bt = Realtek Bluetooth
+# v4l = General catch-all for Video 4 Linux, mainly Conexant hardware.
+# cirrus = CirrusLogic DSP and Amplifier firmware
+# dvb = General catch-all for digital tv adapter interfaces

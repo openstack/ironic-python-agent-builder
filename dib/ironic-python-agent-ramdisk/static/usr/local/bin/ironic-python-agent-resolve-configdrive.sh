96d997f15f9f8a3c8d155a665a2ce0972d34cc61
Mathieu Parent <math.parent@etik.com>
2025-12-06 18:10:40 +0100

    Fix waiting config drive with the correct publisher id
    Local disk can also have a disk with config-2 label
    (typically from the previous provisioning), showing up
    before the correct one (from Virtual Media)
    
    Change-Id: I97f614ab542a781448a138e464656653ade35664
    Signed-off-by: Mathieu Parent <math.parent@etik.com>

--- dib/ironic-python-agent-ramdisk/static/usr/local/bin/ironic-python-agent-resolve-configdrive.sh
+++ dib/ironic-python-agent-ramdisk/static/usr/local/bin/ironic-python-agent-resolve-configdrive.sh
@@ -24,32 +24,21 @@ i=0
 while [ $i -lt 30 ] ; do
     i=$((i + 1))
 
-    # Identify the number of devices
-    device_count=$(lsblk -o PATH,LABEL | grep -c $CONFIG_DRIVE_LABEL || true)
+    for device in $(lsblk -o PATH,LABEL | grep "$CONFIG_DRIVE_LABEL" | cut -d" " -f1); do
+        device_id=$(udevadm info --query=property --property=ID_FS_PUBLISHER_ID "$device" | sed s/ID_FS_PUBLISHER_ID=//)
+        if [[ "${publisher_id,,}" == "${device_id,,}" ]]; then
+            # SUCCESS! Valid device! Mount it!
+            echo "Device ${device} matches publisher id ${publisher_id}. Mounting..."
+            mkdir -p /mnt/config
+            mount -t iso9660 -o ro,mode=0700 "${device}" /mnt/config || true
+            # We've mounted the device, the world is happy.
+            exit 0
+        fi
+    done
 
-    [ $device_count -lt 1 ] || break
     sleep 1
 done
 
-if [ $device_count -lt 1 ]; then
-    # Nothing to do here, exit!
-    exit 0
-else
-    # We have *something* to do here.
-    mkdir -p /mnt/config
-    if [[ "${publisher_id}" != "" ]]; then
-        # We need to enumerate through the devices, and obtain the
-        for device in $(lsblk -o PATH,LABEL|grep config-2|cut -f1 -d" "); do
-            device_id=$(udevadm info --query=property --property=ID_FS_PUBLISHER_ID $device | sed s/ID_FS_PUBLISHER_ID=//)
-            if [[ "${publisher_id,,}" == "${device_id,,}" ]]; then
-                # SUCCESS! Valid device! Do it!
-                echo "Device ${device} matches the ${publisher_id}. Mounting..."
-                mount -t iso9660 -o ro,mode=0700 "${device}" /mnt/config || true
-                # We've mounted the device, the world is happy.
-                exit 0
-            else
-                echo "Did not identify $device as a valid ISO for Ironic."
-            fi
-        done
-    fi
-fi
+# No device found
+echo "No valid configuration drive found for Ironic."
+lsblk -o PATH,LABEL
